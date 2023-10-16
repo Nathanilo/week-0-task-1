b68b34e772fee2afd3ac3a8cda69f601a2c01b81
Nathanilo <nathanielodion@gmail.com>
2023-10-16 22:28:16 +0100

    completed desktop design and algorithms

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,2 +1,13 @@
-function countCharOccurrences(str, char) {}
+function countCharOccurrences(str, char) {
+  let numberOfOccurrence = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (char == str[i]) {
+      numberOfOccurrence++;
+    }
+  }
+
+  return numberOfOccurrence;
+}
+
 module.exports = countCharOccurrences;
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,2 +1,15 @@
-function findSmallest(num1, num2, num3) {}
+function findSmallest(num1, num2, num3) {
+  let smallestNumber;
+
+  if (num1 <= num2 && num1 <= num3) {
+    smallestNumber = num1;
+    return smallestNumber;
+  } else if (num2 <= num1 && num2 <= num3) {
+    smallestNumber = num2;
+    return smallestNumber;
+  } else {
+    smallestNumber = num3;
+    return smallestNumber;
+  }
+}
 module.exports = findSmallest;
